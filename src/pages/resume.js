c4ac7453639d0f9fbe0d260bdabaf3d3a1d42321
Kalvin Chang <kalvin1204@gmail.com>
2020-03-18 00:07:15 +0800

    resume info

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -32,6 +32,32 @@ const AboutPage = () => {
         <Img fluid={data.resumePng.childImageSharp.fluid} id="resume" alt="My resume"></Img>
       </>
     }
+    right={
+      <>
+        <h1>glen a. wilson code</h1>
+        <ul>
+          <li>founder, president</li>
+          <li>first experience with leadership</li>
+        </ul>
+
+        <h1>aftermath</h1>
+        <ul>
+          <li>project lead + full stack</li>
+          <li>first web app</li>
+        </ul>
+
+        <h1>hopscotch health</h1>
+        <ul>
+          <li>software engineering intern</li>
+          <li>first internship</li>
+        </ul>
+
+        <h1>piccollage</h1>
+        <ul>
+          <li>server developer intern</li>
+        </ul>
+      </>
+    }
   />
 }
 
