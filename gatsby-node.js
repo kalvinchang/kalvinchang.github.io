52f2ee29128b7df2517bd2e0a49bd4bdf94f333e
Kalvin Chang <kalvin1204@gmail.com>
2020-03-28 20:45:21 +0800

    pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,27 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path')
+
+// TODO: array?? don't hardcode and get from filepath???
+const pages = {
+  '/': path.resolve("src/paging/index.js"),
+  '/about': path.resolve("src/paging/about.js"),
+  '/timeline': path.resolve("src/paging/timeline.js"),
+  '/contact': path.resolve("src/paging/contact.js"),
+  '/resume': path.resolve("src/paging/resume.js")
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  Object.keys(pages).forEach((route, i) => {
+    createPage({
+      path: route,
+      component: pages[route],
+      context: { currentPage: i + 1 },
+    })
+  })
+}
+
+// need to figure out how to paginate resume and about (should fall under same route but multiple pages within)
