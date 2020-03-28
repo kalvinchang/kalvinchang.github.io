7ee56997d33fe1a2c1c0ec22c98db3117e7c0489
Kalvin Chang <kalvin1204@gmail.com>
2020-03-28 21:02:12 +0800

    rename paging to layouts

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,11 +8,11 @@ const path = require('path')
 
 // TODO: array?? don't hardcode and get from filepath???
 const pages = {
-  '/': path.resolve("src/paging/index.js"),
-  '/about': path.resolve("src/paging/about.js"),
-  '/timeline': path.resolve("src/paging/timeline.js"),
-  '/contact': path.resolve("src/paging/contact.js"),
-  '/resume': path.resolve("src/paging/resume.js")
+  '/': path.resolve("src/layouts/index.js"),
+  '/about': path.resolve("src/layouts/about.js"),
+  '/timeline': path.resolve("src/layouts/timeline.js"),
+  '/contact': path.resolve("src/layouts/contact.js"),
+  '/resume': path.resolve("src/layouts/resume.js")
 }
 
 exports.createPages = ({ graphql, actions }) => {
@@ -22,7 +22,7 @@ exports.createPages = ({ graphql, actions }) => {
     createPage({
       path: route,
       component: pages[route],
-      context: { currentPage: i + 1 },
+      context: { currentPage: i },
     })
   })
 }
