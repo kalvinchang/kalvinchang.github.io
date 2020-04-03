1bf2469a2ca512fc9d851ef95d7eddb5b0127197
Kalvin Chang <kalvin1204@gmail.com>
2020-04-03 20:56:17 +0800

    rename /about/about.js to /about/collections.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,8 +3,9 @@ const path = require('path')
 // TODO: array?? don't hardcode and get from filepath???
 const pages = {
   '/': path.resolve("src/layouts/index.js"),
-  '/about': path.resolve("src/layouts/about/about.js"),
+  '/about': path.resolve("src/layouts/about/collections.js"),
   '/about/timeline': path.resolve("src/layouts/about/timeline.js"),
+  '/blog': path.resolve("src/layouts/blog/entries.js"),
   '/resume': path.resolve("src/layouts/resume/1.js"),
   '/resume/2': path.resolve("src/layouts/resume/2.js"),
   '/resume/3': path.resolve("src/layouts/resume/3.js"),
