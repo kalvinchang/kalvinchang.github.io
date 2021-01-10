41034661073532468eb62313893ba192cf7c0bda
Kalvin Chang <kalvin1204@gmail.com>
2021-01-10 16:07:05 +0800

    content: remove timeline

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,6 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 const pages = {
   '/': path.resolve("src/layouts/index.js"),
   '/about': path.resolve("src/layouts/about/collections.js"),
-  '/about/timeline': path.resolve("src/layouts/about/timeline.js"),
   '/resume': path.resolve("src/layouts/resume/1.js"),
   '/resume/2': path.resolve("src/layouts/resume/2.js"),
   '/resume/3': path.resolve("src/layouts/resume/3.js"),
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Img from "gatsby-image"
 
 // TODO: make global or don't hardcode
-const pages = ['/', '/about', '/about/timeline', '/resume', '/resume/2', '/resume/3', '/resume/4', '/contact', '/blog']
+const pages = ['/', '/about', '/resume', '/resume/2', '/resume/3', '/resume/4', '/contact', '/blog']
 
 const Nav = (props) => {
   // https://nickymeuleman.netlify.com/blog/gatsby-pagination
